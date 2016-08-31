66eaba04c01858bc5a5e741e77159b198822cdd5
tuuleight <adarowski7@gmail.com>
2016-08-31 19:30:23 +0200

    rewritten using asynchronous features

--- auction.py
+++ auction.py
@@ -6,8 +6,9 @@ import os.path
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
-import pymongo
+import motor.motor_tornado
 
+from tornado.concurrent import Future
 from tornado import gen
 from tornado.options import define, options, parse_command_line
 
@@ -15,6 +16,8 @@ define('port', default=8888, help='run on the given port', type=int)
 define('debug', default=True, help='run in debug mode')
 
 executor = concurrent.futures.ThreadPoolExecutor()
+client = motor.motor_tornado.MotorClient()
+db = client.auction_database
 
 
 class Application(tornado.web.Application):
@@ -23,7 +26,7 @@ class Application(tornado.web.Application):
             (r'/', HomeHandler),
             (r'/auction/([^/]+)', AuctionHandler),
             (r'/(\w+)', ProfileHandler),
-            (r'/new', NewHandler),
+            (r'/create/new', NewHandler),
             (r'/auth/create', AuthCreateHandler),
             (r'/auth/login', AuthLoginHandler),
             (r'/auth/logout', AuthLogoutHandler),
@@ -36,49 +39,75 @@ class Application(tornado.web.Application):
             debug=options.debug,
         )
         super(Application, self).__init__(handlers, **settings)
-        client = pymongo.MongoClient('localhost', 27017)
-        self.db = client.auction_database
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    def db(self):
-        return self.application.db
-
     def get_current_user(self):
         user_id = self.get_secure_cookie('auction_user')
         if not user_id:
             return None
         return user_id
 
-    def does_author_exist(self, username):
-        return bool(self.db().users.find_one({'username': username}))
+    @gen.coroutine
+    def does_user_exist(self, username):
+        document = yield db.users.find_one({'username': username})
+        return document
+
+    @gen.coroutine
+    def get_auctions(self):
+        n = yield db.auctions.find().count()
+        auctions = yield db.auctions.find().sort([('start_date',
+                                                   -1)]).to_list(n)
+        return auctions
+
+    @gen.coroutine
+    def get_offers(self, slug):
+        n = yield db.offers.find({'auction_id': slug}).count()
+        offers = yield db.offers.find({'auction_id': slug}).sort([(
+            'price', -1)]).to_list(n)
+        return offers
+
+    @gen.coroutine
+    def find_auctions(self, username):
+        n = yield db.auctions.find({'username': username}).count()
+        auctions = yield db.auctions.find({'username': username}).sort([(
+            'start_date', -1)]).to_list(n)
+        return auctions
+
+    @gen.coroutine
+    def find_offers(self, username):
+        n = yield db.offers.find({'username': username}).count()
+        offers = yield db.offers.find({'username': username}).sort([(
+            'datetime', -1)]).to_list(n)
+        return offers
 
 
 class HomeHandler(BaseHandler):
+    @gen.coroutine
     def get(self):
-        auction = self.db().auctions.find().sort('start_date',
-                                                 pymongo.DESCENDING)
-        if not auction:
-            self.write('No auctions yet. Feel free to register and add one!')
-
+        auction = yield self.get_auctions()
         self.render('index.html', auction=auction)
 
 
 class AuctionHandler(BaseHandler):
+    @gen.coroutine
     def get(self, slug):
-        auction = self.db().auctions.find_one({'_id': ObjectId(slug)})
-        offer = self.db().offers.find({'auction_id': slug})
+        auction = yield db.auctions.find_one({'_id': ObjectId(slug)})
+        offer = yield self.get_offers(slug)
         if not auction:
             raise tornado.web.HTTPError(404)
         self.render('auction_page.html', auction=auction, offer=offer)
 
     @tornado.web.authenticated
+    @gen.coroutine
     def post(self, slug):
-        auction = self.db().auctions.find_one({'_id': ObjectId(slug)})
+        auction = yield db.auctions.find_one({'_id': ObjectId(slug)})
         auction_author = auction['username']
 
         # Unable for auction creator to post offer
-        if self.current_user.decode('utf-8') is not auction_author:
+        if self.current_user.decode('utf-8') == auction_author:
+            self.write('Your own auction')
+        else:
             offer_price = int(self.get_argument('price'))
 
             if offer_price < auction['price_min']:
@@ -91,18 +120,29 @@ class AuctionHandler(BaseHandler):
                     'datetime': datetime.datetime.now(),
                     'auction_id': slug,
                 }
-                self.db().offers.insert_one(offer)
+                # Check whether user already made an offer. If he did,
+                # replace it with the new one.
+                old_offer = yield db.offers.find_one({
+                    'username': offer['username'],
+                    'auction_id': slug
+                })
+
+                if old_offer:
+                    result = yield db.offers.update({'_id': old_offer[
+                        '_id']}, offer)
+                else:
+                    result = yield db.offers.insert(offer)
+
                 self.redirect('/auction/' + slug)
 
 
 class ProfileHandler(BaseHandler):
     @tornado.web.authenticated
+    @gen.coroutine
     def get(self, username):
         if username == self.current_user.decode('utf-8'):
-            user_auctions = self.db().auctions.find({'username': username})
-            u_auction = user_auctions.sort('start_date', pymongo.DESCENDING)
-            user_offers = self.db().offers.find({'username': username})
-            u_offer = user_offers.sort('datetime', pymongo.DESCENDING)
+            u_auction = yield self.find_auctions(username)
+            u_offer = yield self.find_offers(username)
             self.render('profile.html', u_auction=u_auction, u_offer=u_offer)
         else:
             self.write('Sorry, restricted access')
@@ -114,14 +154,15 @@ class NewHandler(BaseHandler):
         self.render('new.html')
 
     @tornado.web.authenticated
+    @gen.coroutine
     def post(self):
         name = self.get_argument('name')
         description = self.get_argument('description')
-        price_min = self.get_argument('price')
+        price_min = int(self.get_argument('price'))
         end_date = self.get_argument('end_date')
 
         new_auction = {
-            'username': self.current_user,
+            'username': self.current_user.decode('utf-8'),
             'name': name,
             'description': description,
             'price_min': price_min,
@@ -129,8 +170,8 @@ class NewHandler(BaseHandler):
             'end_date': end_date
         }
 
-        self.db().auctions.insert_one(new_auction)
-        self.redirect('/')
+        result = yield db.auctions.insert(new_auction)
+        self.redirect(self.get_argument('next', '/'))
 
 
 class AuthCreateHandler(BaseHandler):
@@ -140,7 +181,7 @@ class AuthCreateHandler(BaseHandler):
     @gen.coroutine
     def post(self):
         username = self.get_argument('username')
-        if self.does_author_exist(username):
+        if self.does_user_exist(username):
             raise tornado.web.HTTPError(400, 'user already created')
 
         hashed_password = yield executor.submit(
@@ -148,7 +189,7 @@ class AuthCreateHandler(BaseHandler):
             bcrypt.gensalt())
 
         user = {'username': username, 'password': hashed_password}
-        self.db().users.save(user)
+        db.users.insert(user)
         self.set_secure_cookie('auction_user', username)
         self.redirect(self.get_argument('next', '/'))
 
@@ -160,10 +201,10 @@ class AuthLoginHandler(BaseHandler):
     @gen.coroutine
     def post(self):
         username = self.get_argument('username')
-        if not self.does_author_exist(username):
+        if not self.does_user_exist(username):
             self.redirect('/auth/create')
 
-        user = self.db().users.find_one({'username': username})
+        user = yield db.users.find_one({'username': username})
 
         hashed_password = yield executor.submit(
             bcrypt.hashpw,
@@ -172,7 +213,7 @@ class AuthLoginHandler(BaseHandler):
 
         if hashed_password == user['password']:
             self.set_secure_cookie('auction_user', username)
-            self.render('login.html')
+            self.redirect(self.get_argument('next', '/'))
         else:
             self.render('login.html', error='incorrect password')
 
